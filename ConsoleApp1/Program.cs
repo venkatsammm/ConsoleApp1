83ac7826f6eb74998620cd26438537bdfe38583c
ventramanan <venkat.r@kalvium.community>
2025-04-10 12:01:09 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display "Hello World!" to the console
+            Console.WriteLine("Hello World!");
+
+            // Wait for user to press a key before closing
+            Console.ReadKey();
+        }
+    }
+}
